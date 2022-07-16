6c530070ebc583d746f70167016a59f8b470a3ae
themisch <*>
2022-07-16 17:54:38 +0200

    Adding a complete license notice

--- database.py
+++ database.py
@@ -1,3 +1,20 @@
+"""
+Copyright (C) 2022 themisch
+
+This file is part of Nsc.
+Nsc is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+Nsc is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with Nsc.
+If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import pickle, os, shutil
 from nacl.secret import SecretBox
 
